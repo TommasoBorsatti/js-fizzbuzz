7da84d5bde8e62dadc629f522fdbc92b1b6b3c6a
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-02 14:00:11 +0100

    Script JS base compilato.
    La logica di business dello script dell'esercizio è conclusa. Il programma testato su console.log funziona. Ora procedo a mettere un minimo di css.

--- js/script.js
+++ js/script.js
@@ -10,18 +10,32 @@ Abbiamo visto qualcosa di particolare che possiamo usare?
 
 // 1. Inizializzo un ciclo For che vada da 1 a 100. Una Variabile Numero è definita in base al contatore i.
 
+for ( i = 1; i < 101; i++) {
+
+  var numero = i ;
+
+  //2. SE la variabile Numero è SIA divisibile per 3 CHE divisibile per 5, numero scrive FizzBuzz.
+
+  if (numero % 3 == 0 && numero % 5 == 0) {
+    var numero = "FizzBuzz !!!"
+  }
+
+  //3. SE numero è solo divisibile per 3, numero scrive Fizz.
+
+  if (numero % 3 == 0){
+    var numero = "Fizz !"
+  }
+
+  //4. SE numero è solo divisibile per 5, numero scrive Buzz.
+
+  if (numero % 5 == 0){
+    var numero = "Buzz !!"
+  }
+
+  //5. ALTRIMENTI, in nessun caso dei precedenti, numero renderà i come stabilito all'inizio.
+
+  // 6. il controllo del ciclo termina con la stampa, prima di ripetersi per l'i successivo.
+  console.log(numero)
 
 
-//2. SE la variabile Numero è SIA divisibile per 3 CHE divisibile per 5, numero scrive FizzBuzz.
-
-
-
-//3. SE ALTRIMENTI numero è solo divisibile per 3, numero scrive Fizz.
-
-
-
-//4. SE ALTRIMENTI numero è solo divisibile per 5, numero scrive Buzz.
-
-
-
-//5. ALTRIMENTI, in nessun caso dei precedenti, numero renderà numero.
+}
