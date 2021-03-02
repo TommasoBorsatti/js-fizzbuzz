50f2005999c275837ff772be3b26edff6df267e3
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-03-02 13:41:24 +0100

    HTML, style e script pronti
    Tracciate le milestone che dovrà avere la logica di business dello script

---
+++ js/script.js
@@ -0,0 +1,27 @@
+
+/*
+Scrivi un programma che stampi i numeri da 1 a 100,
+ma per i multipli di 3 stampi “Fizz” al posto del numero e per i multipli di 5 stampi Buzz.
+Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
+Prima di partire a scrivere codice poniamoci qualche domanda:
+Come faccio a sapere se un numero è divisibile per?
+Abbiamo visto qualcosa di particolare che possiamo usare?
+*/
+
+// 1. Inizializzo un ciclo For che vada da 1 a 100. Una Variabile Numero è definita in base al contatore i.
+
+
+
+//2. SE la variabile Numero è SIA divisibile per 3 CHE divisibile per 5, numero scrive FizzBuzz.
+
+
+
+//3. SE ALTRIMENTI numero è solo divisibile per 3, numero scrive Fizz.
+
+
+
+//4. SE ALTRIMENTI numero è solo divisibile per 5, numero scrive Buzz.
+
+
+
+//5. ALTRIMENTI, in nessun caso dei precedenti, numero renderà numero.
